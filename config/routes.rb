b46111f5df60b4443665a1e0513d930a218e6c6f
marihan <marihan.girgis@gmail.com>
2018-03-11 06:18:35 +0200

    add facebook authorized

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
   resources :inviteds
   resources :friendships
   resources :orders
-  devise_for :users
-  get 'home/index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  authenticated :user do
+   root 'home#index', as: 'authenticated_root'
+  end
+ devise_scope :user do
+   root 'devise/sessions#new'
+  end
   resources :users
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
