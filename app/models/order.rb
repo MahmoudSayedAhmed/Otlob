2bcdb415a835c4cfe1212d3321a0f88e3dce96c2
MahmoudSayedAhmed <mahmoud.maghni8@gmail.com>
2018-03-15 21:08:00 +0200

    adding ws

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :inviteds
+  has_many :inviteds, dependent: :delete_all
   has_many :joineds
-
+  
   has_attached_file :Menu, styles: { larg:"600x600>", medium: "300x300>", thumb: "50x50#" }
   validates_attachment_content_type :Menu, content_type: /\Aimage\/.*\z/
-
 
 end
