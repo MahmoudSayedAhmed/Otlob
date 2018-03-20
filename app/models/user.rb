934dc5bb73f5872e704b1de5f028a2575edc7f2a
Eslam1141 <eslamabdallah1419@outlook.com>
2018-03-20 21:16:10 +0200

    fixing bugs for sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,6 @@ class User < ApplicationRecord
         # user.image = auth.info.image # assuming the user model has an image
       end
     end
-  before_create { generate_token(:auth_token) }
   
 def send_password_reset
   generate_token(:password_reset_token)
